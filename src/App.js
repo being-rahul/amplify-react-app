42501bd09dee175be29c626f85b071a3a596904d
Rahul Yadav <38113367+being-rahul@users.noreply.github.com>
2020-12-30 17:18:33 +0530

    just 1 line changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi this is rahul yadav, new to react and amplify
         </p>
         <a
           className="App-link"
